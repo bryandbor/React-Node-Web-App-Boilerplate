c38629bf3ad877f862cc72daaa96233f0e9c3e23
Bryan Bor <bryandbor@gmail.com>
2017-03-03 20:59:48 -0800

    Bugfix - “Root” Link Not Found ROOT was removed in the last update to simplify the file structure.

--- client/src/views/NoMatch.js
+++ client/src/views/NoMatch.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import FA from 'react-fontawesome';
 
-import { ROOT } from '../routes';
-
 class NoMatch extends Component {
   render() {
     return (
       <div id="NoMatch">
-      	<Link to={ROOT}>
+      	<Link to='/'>
       		<FA name="caret-left"/> Back
   			</Link>
       	&nbsp;No match found for path {this.props.location.pathname}
